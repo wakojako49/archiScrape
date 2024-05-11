7e008ecc14db9c8430cafc37d07b70485698d448
wakojako49 <jackdumanat49@gmail.com>
2024-05-11 21:55:32 +1000

    commit

--- main2.py
+++ main2.py
@@ -6,70 +6,40 @@ from post_code import post_code
 import csv
 import logging
 import re
-import datetime
 
 # Set up logging
 logger = logging.getLogger(__name__)
 logging.basicConfig(filename='archiScrape.log', level=logging.INFO)
 
-class scraper():
-    iter_number = 1
-    def __init__(self) -> None:
-        scraper.codes = 2204
-        scraper.url= f"https://www.architects.nsw.gov.au/component/arbregister/?view=architects&regSearchSuburb={scraper.codes}&Itemid=144&start={self.iter_number}"
-        scraper._x = requests.get(scraper.url)
-        scraper._soup = BeautifulSoup(scraper._x.text, 'html.parser')
-
-        def get_page_numbers():
-            page_numbers = scraper._soup.find("p", class_="counter")
-
-            #should capture number of pages.
-            try:
-                test = page_numbers.get_text(strip=True)
-                page_iter = re.search(r'\d+$', test).group(0)
-                logger.info(f"{datetime.datetime.now()}: Post code {self.codes} has only {self.page_iter} page.")
-
-            except:
-                self.page_iter = 1
-                logger.info(f"{datetime.datetime.now()}: Post code {scraper.codes} has only 1 page.")
-
-            return page_iter
-        scraper.page_iter = get_page_numbers()
 
 
-
-
-#codes = post_code[random.randint(0, len(post_code))]
-# codes = 2204
-# iter_number = 1
-# x = requests.get(f"https://www.architects.nsw.gov.au/component/arbregister/?view=architects&regSearchSuburb={codes}&Itemid=144&start={iter_number}")
-# soup = BeautifulSoup(x.text, 'html.parser')
-# page_numbers = soup.find("p", class_="counter")
+codes = post_code[100]
+iter_number = 1
+x = requests.get(f"https://www.architects.nsw.gov.au/component/arbregister/?view=architects&regSearchSuburb={codes}&start={iter_number}")
+soup = BeautifulSoup(x.text, 'html.parser')
+page_numbers = soup.find("p", class_="counter")
 
 #should capture number of pages.
-# try:
-#     test = page_numbers.get_text(strip=True)
-#     page_iter = re.search(r'\d+$', test).group(0)
-#     logger.info(f"{datetime.datetime.now()}: Post code {codes} has only {page_iter} page.")
+try:
+    test = page_numbers.get_text(strip=True)
+    page_iter = re.search(r'\d+$', test).group(0)
+except:
+    logger.info(f"Post code {codes} has only one page.")
 
-# except:
-#     page_iter = 1
-#     logger.info(f"{datetime.datetime.now()}: Post code {codes} has only 1 page.")
-
-a = scraper()
-
-print(a.page_iter)
-print(len(a._soup.find_all('tr')))
-print(a.url)
+# for iter_codes, codes in enumerate(post_code):
+#     logger.info(f"Searching for post code: {codes}")
+#     iter_number = 1
+#     x = requests.get(f"https://www.architects.nsw.gov.au/component/arbregister/?view=architects&regSearchSuburb={codes}&Itemid=144&start={iter_number}")
+#     soup = BeautifulSoup(x.text, 'html.parser')
+#     page_numbers = soup.find("p", class_="counter")
 
 
 
-
-with open('output.csv', 'a', newline='') as f:
+with open('output.csv', 'w', newline='') as f:
     writer = csv.writer(f)
 
     # Find all 'tr' elements
-    for tr in a._soup.find_all('tr'):
+    for tr in soup.find_all('tr'):
         row = []
 
         # For each 'tr', find all 'td' elements within it
@@ -82,10 +52,3 @@ with open('output.csv', 'a', newline='') as f:
 
         # Write the list to the CSV file
         writer.writerow(row)
-
-        # for iter_codes, codes in enumerate(post_code):
-#     logger.info(f"Searching for post code: {codes}")
-#     iter_number = 1
-#     x = requests.get(f"https://www.architects.nsw.gov.au/component/arbregister/?view=architects&regSearchSuburb={codes}&Itemid=144&start={iter_number}")
-#     soup = BeautifulSoup(x.text, 'html.parser')
-#     page_numbers = soup.find("p", class_="counter")
--- main3.py
+++ main3.py
@@ -15,31 +15,70 @@ logging.basicConfig(filename='archiScrape.log', level=logging.INFO)
 
 iter_number = 1
 # post_codes = post_code[random.randint(0, len(post_code))]
-post_codes = 2121
+post_codes = 2000
 
-url= f"https://www.architects.nsw.gov.au/component/arbregister/?view=architects&regSearchSuburb={post_codes}"
+url = f"https://www.architects.nsw.gov.au/component/arbregister/?view=architects&regSearchSuburb={post_codes}"
 x = requests.get(url)
 soup = BeautifulSoup(x.text, 'html.parser')
+print(type(x))
+print(type(soup))
 
 
-# next = soup.find_all('a', title='Next', href=True)
-# print(next['href'])
+def get_href(class_name) -> str :
+    '''
+        returns next link page if not returns ""
+            Parameters:
+                class_name: BeautifulSoup object
 
-def get_href(class_name):
+            Returns:
+                next_link: str
+    '''
+    next_link:str = ""
     for a in class_name.find_all('a', title='Next', href=True):
-        try:
-            return(a['href'])
-        except:
-            raise Exception("No href found")
+        if a['href'] is None:
+            next_link = ""
+        else:
+            next_link = a['href']
+    return next_link
 
-    
-print(url+get_href(soup))
-# print(get_href(soup))
 
-# create for loop to iterate through pages
-# grab href from <a> of any class="pagination-next"
+def write_rows(class_name):
+    '''
+        writes rows to csv file
+            Parameters:
+                class_name: BeautifulSoup object
+    '''
+    with open('output.csv', 'a', newline='') as f:
+        writer = csv.writer(f)
+        for tr in class_name.find_all('tr'):
+            row = []
+            for td in tr.find_all('td'):
+                row.append(td.get_text(strip=True))
+            for links in tr.find_all('a'):
+                row.append(links.get('href'))
+            writer.writerow(row)        
+            
+def check_tr(class_name)-> bool:
+    '''
+        checks if there is any 'tr' in the class
+            Parameters:
+                class_name: BeautifulSoup object
+    '''
+    if class_name.find_all('tr') == []:
+        return False
+    else:
+        return True
+
 
 
 
 # function that grabs "td in tr.find_all('td')"
 # then grabs "a" and "href"
+
+if get_href(soup) is None:
+    logger.info(f"Post code {post_codes} has only one page.")
+else:
+    print(url+get_href(soup))
+    
+write_rows(soup)
+print(check_tr(soup))
