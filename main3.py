1ffeeed2c1b210b20fe5fddc269766e3d29580ca
wakojako49 <jackdumanat49@gmail.com>
2024-05-12 22:05:14 +1000

    update

--- main3.py
+++ main3.py
@@ -15,18 +15,16 @@ logging.basicConfig(filename='archiScrape.log', level=logging.INFO)
 
 iter_number = 1
 # post_codes = post_code[random.randint(0, len(post_code))]
-post_codes = 2000
+post_codes = 2121
 
 url = f"https://www.architects.nsw.gov.au/component/arbregister/?view=architects&regSearchSuburb={post_codes}"
 x = requests.get(url)
 soup = BeautifulSoup(x.text, 'html.parser')
-print(type(x))
-print(type(soup))
 
 
-def get_href(class_name) -> str :
+def get_next(class_name) -> str :
     '''
-        returns next link page if not returns ""
+        returns next page url if exists, otherwise returns ""
             Parameters:
                 class_name: BeautifulSoup object
 
@@ -36,11 +34,14 @@ def get_href(class_name) -> str :
     next_link:str = ""
     for a in class_name.find_all('a', title='Next', href=True):
         if a['href'] is None:
-            next_link = ""
+            next_link = "" 
         else:
             next_link = a['href']
     return next_link
 
+#TODO: write_rows() may need to be modified to a different function
+# It needs to get links from each architect
+# then go into it an grab the information
 
 def write_rows(class_name):
     '''
@@ -68,17 +69,29 @@ def check_tr(class_name)-> bool:
         return False
     else:
         return True
+    
+
+# create while equence:
+# 1. run write_rows()
+# 2. change url to url+get_next(soup)
+# 3. if check_tr(soup) is False, break 
+
+while get_next(soup) != "":
+    write_rows(soup)
+    print(url+get_next(soup))
+    x = requests.get(url+get_next(soup))
+    soup = BeautifulSoup(x.text, 'html.parser')
+    time.sleep(random.randint(1, 5))
+    #iter_number += 1
+    if check_tr(soup) == False:
+        break
 
 
 
-
-# function that grabs "td in tr.find_all('td')"
-# then grabs "a" and "href"
-
-if get_href(soup) is None:
+if get_next(soup) is None:
     logger.info(f"Post code {post_codes} has only one page.")
 else:
-    print(url+get_href(soup))
-    
+    print(url+get_next(soup))
+
 write_rows(soup)
 print(check_tr(soup))
